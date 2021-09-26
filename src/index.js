f1de453592514986f8cef9aab29aef2c46b72830
Joe.Shieh <b97502027@gmail.com>
2021-09-26 20:07:05 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import WeatherApp from "./WeatherApp";
+
+// 這支 CSS 檔的樣式會作用到全域
+import "./styles.css";
+
+function App() {
+  return <WeatherApp />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
